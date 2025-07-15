978298d9d047de33d88e50bff9b20477df960e95
lbruesehoff <55064350+lbruesehoff@users.noreply.github.com>
2025-07-14 21:41:34 -0500

    sing-up page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import { Routes, Route } from "react-router-dom";
+import SignUp from "./pages/Sign-Up/SignUp";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/sign-up" element={<SignUp />} />
         {/* Add more routes here as needed */}
       </Routes>
     </div>
---
+++ src/pages/Sign-Up/SignUp.tsx
@@ -0,0 +1,115 @@
+import React from "react";
+import "./SignUp.scss";
+import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+
+const SignUp: React.FC = () => {
+  const {
+    register,
+    handleSubmit,
+    getValues,
+    setError,
+    formState: { errors },
+  } = useForm();
+  const navigate = useNavigate();
+
+  const onSubmit = () => {
+    navigate("/home");
+  };
+  return (
+    <div className="sign-up-page">
+      <div className="sign-up-form-container">
+        <div className="sign-up-title">Create an Account</div>
+        <form
+          action=""
+          className="sign-up-form"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <label className="input validator">
+            <svg
+              className="h-[1em] opacity-50"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <g
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                strokeWidth="2.5"
+                fill="none"
+                stroke="currentColor"
+              >
+                <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"></path>
+                <circle cx="12" cy="7" r="4"></circle>
+              </g>
+            </svg>
+            <input
+              type="text"
+              required
+              placeholder="First Name"
+              pattern="[A-Za-z][A-Za-z0-9\-]*"
+              minLength={3}
+              maxLength={30}
+              title="Only letters, numbers or dash"
+            />
+          </label>
+          <label className="input validator">
+            <svg
+              className="h-[1em] opacity-50"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <g
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                strokeWidth="2.5"
+                fill="none"
+                stroke="currentColor"
+              >
+                <rect width="20" height="16" x="2" y="4" rx="2"></rect>
+                <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
+              </g>
+            </svg>
+            <input type="email" placeholder="Email" />
+          </label>
+          <label className="input validator">
+            <svg
+              className="h-[1em] opacity-50"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <g
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                strokeWidth="2.5"
+                fill="none"
+                stroke="currentColor"
+              >
+                <path d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"></path>
+                <circle cx="16.5" cy="7.5" r=".5" fill="currentColor"></circle>
+              </g>
+            </svg>
+            <input
+              type="password"
+              placeholder="Password"
+              minLength={8}
+              pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+              title="Must be more than 8 characters, including number, lowercase letter, uppercase letter"
+            />
+          </label>
+          <div className="link-buttons">
+            <a href="/forgot-password" className="forgot-password-link"></a>
+            <a href="" className="sign-up-link">
+              Already have an account? Log in
+            </a>
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Sign Up
+          </button>
+        </form>
+      </div>
+      <div className="sign-up-image-container"></div>
+    </div>
+  );
+};
+
+export default SignUp;
